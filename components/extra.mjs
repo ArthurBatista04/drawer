ad678c4d46fd9b65b6ab3bb7c08e72d989e29d2c
arthurbatista04 <arthurbatista04@outlook.com>
2020-01-21 14:59:54 -0300

    keybinding

--- components/extra.mjs
+++ components/extra.mjs
@@ -17,8 +17,15 @@ export default class Extras {
   clear() {
     this.store.dispatch({ type: "CLEAR" });
   }
-
+  logKey(e) {
+    if (e.key === "z") {
+      this.undo();
+    } else if (e.key === "y") {
+      this.redo();
+    }
+  }
   addEvents() {
+    document.addEventListener("keydown", this.logKey.bind(this), false);
     this.$undo.addEventListener("click", this.undo.bind(this), false);
     this.$redo.addEventListener("click", this.redo.bind(this), false);
     this.$clear.addEventListener("click", this.clear.bind(this), false);
