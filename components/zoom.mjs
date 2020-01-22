7d8f864876a765eca7102cf3bbd4cb14ed05fd9e
deelefrati <douglasdelefrati@gmail.com>
2020-01-22 20:35:23 -0300

    zoom extend working

--- components/zoom.mjs
+++ components/zoom.mjs
@@ -1,91 +1,151 @@
-import { getExtremePoints, getPresentState } from "../stores/connect.mjs";
-import { multiply } from "../utils/index.mjs";
+import {getExtremePoints, getPresentState} from "../stores/connect.mjs";
+import {multiply, ToastMessage} from "../utils/index.mjs";
+import Coordinate from "./coordinate.mjs";
+
 export default class Zoom {
-  constructor(store) {
-    this.store = store;
-    this.$zoom = document.getElementById("zoom");
-    this.$zoomExtend = document.getElementById("zoomExtend");
-  }
+	constructor(store) {
+		this.store = store;
+		this.coordinates = new Coordinate();
+		this.points = [];
+		this.point = null;
+		this.isTransformation = false;
+		this.$zoom = document.getElementById("zoom");
+		this.$zoomExtend = document.getElementById("zoomExtend");
+		this.$zoomIn = document.getElementById("zoomIn");
+		this.$canvas = document.getElementById("canvas");
+	}
 
-  zoomExtend() {
-    const shapes = getPresentState(this.store);
-    const points = getExtremePoints(this.store);
+	callZoomExtend() {
+		const points = getExtremePoints(this.store);
+		this.zoomExtend(points);
+	}
 
-    const x = { min: points[2], max: points[3] };
-    const y = { min: points[0], max: points[1] };
-    const u = { min: 0, max: 1300 };
-    const v = { min: 0, max: 820 };
-    const sx = (u.max - u.min) / (x.max - x.min);
-    const sy = (v.max - v.min) / (y.max - y.min);
-    const rw = (x.max - x.min) / (y.max - y.min);
-    const rv = (u.max - u.min) / (v.max - v.min);
-    rw > rv
-      ? (v.newMax = (u.max - u.min) / rw + v.min)
-      : (u.newMax = rw * (v.max - v.min) + u.min);
-    if (rw > rv) {
-      for (const [id, value] of Object.entries(shapes)) {
-        // let newPoints = value.points.map(point => {
-        //   return {
-        //     x: point.x * sx - sx * x.min,
-        //     y: point.y * sy - sy * y.min - (v.max - v.newMax) / 2
-        //   };
-        // });
-        const line1 = [sx, 0, -sx * x.min];
-        const line2 = [0, sy, -sy * y.min - (v.max - v.newMax) / 2];
-        const line3 = [0, 0, 1];
-        let matrixPoints = [[], [], []];
-        for (const point of value.points) {
-          matrixPoints[0].push(point.x);
-          matrixPoints[1].push(point.y);
-          matrixPoints[2].push(1);
-        }
-        const result = multiply([line1, line2, line3], matrixPoints);
-        let newPoints = [];
-        for (let i = 0; i < value.points.length; i++) {
-          newPoints.push({ x: result[0][i] });
-        }
-        for (let i = 0; i < value.points.length; i++) {
-          newPoints[i]["y"] = result[1][i];
-        }
-        this.store.dispatch({ type: "UPDATE", points: newPoints, id: id });
-      }
-    } else {
-      for (const [id, value] of Object.entries(shapes)) {
-        // let newPoints = value.points.map(point => {
-        //   return {
-        //     x: point.x * sx - sx * x.min - (u.max - u.newMax) / 2,
-        //     y: point.y * sy - sy * y.min
-        //   };
-        // });
-        const line1 = [sx, 0, -sx * x.min - (u.max - u.newMax) / 2];
-        const line2 = [0, sy, -sy * y.min];
-        const line3 = [0, 0, 1];
-        let matrixPoints = [[], [], []];
-        for (const point of value.points) {
-          matrixPoints[0].push(point.x);
-          matrixPoints[1].push(point.y);
-          matrixPoints[2].push(1);
-        }
-        const result = multiply([line1, line2, line3], matrixPoints);
-        let newPoints = [];
-        for (let i = 0; i < value.points.length; i++) {
-          newPoints.push({ x: result[0][i] });
-        }
-        for (let i = 0; i < value.points.length; i++) {
-          newPoints[i]["y"] = result[1][i];
-        }
+	zoomExtend(points) {
+		const shapes = getPresentState(this.store);
 
-        this.store.dispatch({ type: "UPDATE", points: newPoints, id: id });
-      }
-    }
-    //
-  }
-  addEvents() {
-    this.$zoomExtend.addEventListener(
-      "click",
-      this.zoomExtend.bind(this),
-      false
-    );
-    // this.$zoom.addEventListener("click", this.zoom.bind(this));
-  }
+		const x = {min: points[2], max: points[3]};
+		const y = {min: points[0], max: points[1]};
+		const u = {min: 0, max: this.$canvas.width};
+		const v = {min: 0, max: this.$canvas.height};
+		const rw = (x.max - x.min) / (y.max - y.min);
+		const rv = (u.max - u.min) / (v.max - v.min);
+
+		rw > rv
+			? (v.newMax = (u.max - u.min) / rw + v.min)
+			: (u.newMax = rw * (v.max - v.min) + u.min);
+
+		let sy = 0;
+		let sx = 0;
+
+		if (rw > rv) {
+			sy = (v.newMax - v.min) / (y.max - y.min);
+			sx = (u.max - u.min) / (x.max - x.min);
+		} else {
+			sy = (v.max - v.min) / (y.max - y.min);
+			sx = (u.newMax - u.min) / (x.max - x.min);
+		}
+		const line1 = [sx, 0, -sx * x.min];
+		const line2 = [0, sy, -sy * y.min];
+		const line3 = [0, 0, 1];
+		if (rw > rv) {
+			const line4 = [1, 0, 0];
+			const line5 = [0, 1, (v.max - v.newMax) / 2];
+			const line6 = [0, 0, 1];
+			for (const [id, value] of Object.entries(shapes)) {
+				let matrixPoints = [[], [], []];
+				console.log(value.points);
+				for (const point of value.points) {
+					matrixPoints[0].push(point.x);
+					matrixPoints[1].push(point.y);
+					matrixPoints[2].push(1);
+				}
+				console.log(matrixPoints);
+				let result = multiply([line1, line2, line3], matrixPoints);
+				result = multiply([line4, line5, line6], result);
+				let newPoints = [];
+				for (let i = 0; i < value.points.length; i++) {
+					newPoints.push({x: result[0][i]});
+				}
+				console.log(newPoints);
+				for (let i = 0; i < value.points.length; i++) {
+					newPoints[i]["y"] = result[1][i];
+				}
+
+				this.store.dispatch({type: "UPDATE", points: newPoints, id: id});
+			}
+		} else {
+			const line4 = [1, 0, (u.max - u.newMax) / 2];
+			const line5 = [0, 1, 0];
+			const line6 = [0, 0, 1];
+			for (const [id, value] of Object.entries(shapes)) {
+				let matrixPoints = [[], [], []];
+				for (const point of value.points) {
+					matrixPoints[0].push(point.x);
+					matrixPoints[1].push(point.y);
+					matrixPoints[2].push(1);
+				}
+				let result = multiply([line1, line2, line3], matrixPoints);
+				result = multiply([line4, line5, line6], result);
+				let newPoints = [];
+				for (let i = 0; i < value.points.length; i++) {
+					newPoints.push({x: result[0][i]});
+				}
+				for (let i = 0; i < value.points.length; i++) {
+					newPoints[i]["y"] = result[1][i];
+				}
+
+				this.store.dispatch({type: "UPDATE", points: newPoints, id: id});
+			}
+		}
+	}
+
+	message(message) {
+		ToastMessage.fire({
+			icon: "info",
+			title: message
+		});
+	}
+
+	addPoint() {
+		if (this.isTransformation) {
+			this.points.push(this.point);
+			if (this.points.length == 2) {
+				this.zoom();
+			}
+		}
+	}
+
+	executeZoom() {
+		const shapes = getPresentState(this.store);
+		this.zoomExtend(this.points);
+	}
+
+	zoom(points) {
+		console.log(points);
+		this.isTransformation = true;
+		if (points.length === 2) {
+			this.isTransformation = false;
+			this.executeZoom(this.points);
+		} else {
+			const message = "selecione dois pontos";
+			this.message(message);
+		}
+	}
+	addEvents() {
+		this.$zoomExtend.addEventListener(
+			"click",
+			this.callZoomExtend.bind(this),
+			false
+		);
+		this.$zoomIn.addEventListener("click", this.zoom.bind(this), false);
+		this.$canvas.addEventListener("click", this.addPoint.bind(this), false);
+		this.$canvas.addEventListener(
+			"mousedown",
+			e => {
+				this.point = this.coordinates.getMousePos(this.$canvas, e);
+			},
+			false
+		);
+		// this.$zoom.addEventListener("click", this.zoom.bind(this));
+	}
 }
--- index.mjs
+++ index.mjs
@@ -1,12 +1,12 @@
 import configureStore from "./stores/configureStore.mjs";
 import {
-  Coordinate,
-  CanvasBuilder,
-  Extras,
-  ListBuilder,
-  Shapes,
-  Transformations,
-  Zoom
+	Coordinate,
+	CanvasBuilder,
+	Extras,
+	ListBuilder,
+	Shapes,
+	Transformations,
+	Zoom
 } from "./components/index.mjs";
 
 const store = configureStore();
@@ -26,59 +26,66 @@ canvasBuilder.init();
 listBuilder.init();
 
 function create_line(x, y, x2, y2) {
-  const point1 = { x: x, y: y };
-  const point2 = { x: x2, y: y2 };
-  shapes.shape = "LINE";
-  shapes.points = [point1, point2];
-  shapes.create();
+	const point1 = {x: x, y: y};
+	const point2 = {x: x2, y: y2};
+	shapes.shape = "LINE";
+	shapes.points = [point1, point2];
+	shapes.create();
 }
 
 function create_triangle(x, y, x2, y2, x3, y3) {
-  const point1 = { x: x, y: y };
-  const point2 = { x: x2, y: y2 };
-  const point3 = { x: x3, y: y3 };
-  shapes.shape = "TRIANGLE";
-  shapes.points = [point1, point2, point3];
-  shapes.create();
+	const point1 = {x: x, y: y};
+	const point2 = {x: x2, y: y2};
+	const point3 = {x: x3, y: y3};
+	shapes.shape = "TRIANGLE";
+	shapes.points = [point1, point2, point3];
+	shapes.create();
 }
 
 function create_square(x, y, x2, y2) {
-  const point1 = { x: x, y: y };
-  const point2 = { x: x2, y: y2 };
-  shapes.shape = "SQUARE";
-  shapes.points = [point1, point2];
-  shapes.create();
+	const point1 = {x: x, y: y};
+	const point2 = {x: x2, y: y2};
+	shapes.shape = "SQUARE";
+	shapes.points = [point1, point2];
+	shapes.create();
 }
 
 function create_circle(x, y, x2, y2) {
-  const point1 = { x: x, y: y };
-  const point2 = { x: x2, y: y2 };
-  shapes.shape = "CIRCLE";
-  shapes.points = [point1, point2];
-  shapes.create();
+	const point1 = {x: x, y: y};
+	const point2 = {x: x2, y: y2};
+	shapes.shape = "CIRCLE";
+	shapes.points = [point1, point2];
+	shapes.create();
 }
 
 function select_shape(id) {
-  listBuilder.select(id.toString());
+	listBuilder.select(id.toString());
 }
 
 function delete_shape(id) {
-  listBuilder.delete(id.toString());
+	listBuilder.delete(id.toString());
 }
 function translate(x1, y1, x2, y2) {
-  const point1 = { x: x1, y: y1 };
-  const point2 = { x: x2, y: y2 };
-  transformations.translate(point2, point1);
+	const point1 = {x: x1, y: y1};
+	const point2 = {x: x2, y: y2};
+	transformations.translate(point2, point1);
 }
 
 function rotate(x, y, angle) {
-  const point1 = { x: x, y: y };
-  transformations.rotate(angle, point1);
+	const point1 = {x: x, y: y};
+	transformations.rotate(angle, point1);
 }
 
 function scale(x, y) {
-  const scales = { x: x, y: y };
-  transformations.scale(scales);
+	const scales = {x: x, y: y};
+	transformations.scale(scales);
+}
+
+function zoomIn(x1, y1, x2, y2) {
+	const point1 = {x: x1, y: y1};
+	const point2 = {x: x2, y: y2};
+
+	zoom.zoom([point1, point2]);
 }
 
 window.create_line = create_line;
@@ -90,3 +97,4 @@ window.delete_shape = delete_shape;
 window.translate = translate;
 window.rotate = rotate;
 window.scale = scale;
+window.zoom_in = zoomIn;
